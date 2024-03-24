303aaa434179bcbbdbc0200c148f63afff97c587
malindev07 <malindev07@yandex.ru>
2024-03-25 00:48:50 +0300

    Фикс ошибок

--- func_handlers/action_data.py
+++ func_handlers/action_data.py
@@ -1,63 +1,96 @@
 import json
+from typing import List, Optional
+
 import pyshorteners
+from pathlib import *
+from pydantic import BaseModel
+from pydantic_core import core_schema
 
 
-def save_data(data_url: dict) -> str:
-    with open('data/data_base.json', 'r') as open_file:
-        data_base = json.load(open_file)
-        if data_url in data_base['urls']:
-            return ('Ccылка уже сущестсвует!')
-        data_base['urls'].append(data_url)
-        with open('data/data_base.json', 'w') as out_file:
-            json.dump(data_base, out_file, indent=4)
-            return 'Ссылка добавлена!'
+class DataJSON(BaseModel):
+    long_url: str
+    short_url: str
 
 
-def delete_data(short_url: str = '', long_url: str = ''):
-    with open('data/data_base.json', 'r') as open_file:
-        data_base = json.load(open_file)
-        filtered_value = [
-            *filter(lambda elem: elem['short_url'] == short_url or elem['long_url'] == long_url,
-                    data_base['urls'])]
+class DataURLS(BaseModel):
+    urls: List[DataJSON] = []
+
+
+def save_data(long_url: str) -> DataURLS:
+    data_base = DataURLS()
+    data = DataJSON(long_url=long_url, short_url=make_short_url(long_url))
+
+    if get_path_db().stat().st_size == 0:
+        with open(get_path_db(), 'w') as open_file:
+            data_base.urls.append(data.__dict__)
+            json.dump(data_base.__dict__, open_file, indent=4)
+    else:
+        with open(get_path_db(), 'r') as read_file:
+            data_base.urls = [DataJSON(**elem).__dict__ for elem in json.load(read_file)['urls']]
+
+            if data.__dict__ not in data_base.urls:
+                data_base.urls.append(data.__dict__)
+
+                with open(get_path_db(), 'w') as out_file:
+                    json.dump(data_base.__dict__, out_file, indent=4)
+
+    return data_base
+
+
+def delete_data(url: str) -> DataURLS:
+    data_base = DataURLS()
+
+    if get_path_db().stat().st_size != 0:
+        with open(get_path_db(), 'r') as open_file:
+            data_base.urls = [DataJSON(**elem).__dict__ for elem in json.load(open_file)['urls']]
+
+            filtered_value = [*filter(lambda elem: elem['short_url'] == url or elem['long_url'] == url, data_base.urls)]
+
+            if filtered_value:
+                data_base.urls.remove(*filtered_value)
+
+            with open(get_path_db(), 'w') as out_file:
+                json.dump(data_base.__dict__, out_file, indent=4)
+
+    return data_base
+
+
+def update_data(new_url: str, old_url: str) -> DataURLS:
+    with open(get_path_db(), 'r') as open_file:
+        data_base = DataURLS()
+        data_base.urls = [DataJSON(**elem).__dict__ for elem in json.load(open_file)['urls']]
+
+        filtered_value = [*filter(lambda elem: elem['long_url'] == old_url, data_base.urls)]
 
         if filtered_value:
-            data_base['urls'].remove(filtered_value[0])
-            with open('data/data_base.json', 'w') as out_file:
-                json.dump(data_base, out_file, indent=4)
-            return 'Ссылка удалена!', filtered_value[0]
-        else:
-            return 'Ссылка не найдена!', filtered_value
-
-
-def update_data(new_url: str, old_url: str):
-    with open('data/data_base.json', 'r') as open_file:
-        data_base = json.load(open_file)
-
-        if [*filter(lambda elem: elem['long_url'] == old_url, data_base['urls'])]:
-            filtered_value = [*filter(lambda elem: elem['long_url'] == old_url, data_base['urls'])]
-
-            data_base['urls'].remove(filtered_value[0])
+            data_base.urls.remove(filtered_value[0])
 
             filtered_value[0]['long_url'] = new_url
             filtered_value[0]['short_url'] = make_short_url(new_url)
 
-            data_base['urls'].append(filtered_value[0])
+            data_base.urls.append(filtered_value[0])
 
-            with open('data/data_base.json', 'w') as out_file:
-                json.dump(data_base, out_file, indent=4)
-                return 'Ссылка обновлена!', filtered_value
+        with open(get_path_db(), 'w') as out_file:
+            json.dump(data_base.__dict__, out_file, indent=4)
 
-        else:
-            return 'Ссылка не найдена'
+    return data_base
 
 
-def show_all_urls():
-    with open('data/data_base.json', 'r') as open_file:
-        data_base = json.load(open_file)
-        return data_base
+def show_all_urls() -> DataURLS:
+    data_base = DataURLS()
+
+    with open(get_path_db(), 'r') as open_file:
+        data_base.urls = [DataJSON(**elem).__dict__ for elem in json.load(open_file)['urls']]
+
+    return data_base
 
 
 def make_short_url(long_url: str) -> str:
     shortener = pyshorteners.Shortener()
     short_url = shortener.tinyurl.short(long_url)
     return short_url
+
+
+def get_path_db() -> Path:
+    data_path = Path.cwd().joinpath('data').joinpath('data_base.json')
+    return data_path
--- main.py
+++ main.py
@@ -1,39 +1,25 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from func_handlers.action_data import save_data, delete_data, update_data, make_short_url, show_all_urls
+
+from func_handlers.action_data import (save_data, delete_data, update_data, show_all_urls, DataJSON, DataURLS)
 
 app = FastAPI()
 
 
-class DataJSON(BaseModel):
-    long_url: str
-    short_url: str
+@app.post('/')
+def make_short_url_handler(long_url: str) -> DataURLS:
+    return save_data(long_url)
 
 
-@app.post('/make_short_url')
-def make_short_url_handler(long_url: str):
-    data = DataJSON
-
-    data.long_url = long_url
-    data.short_url = make_short_url(long_url)
-    result = {'long_url': data.long_url, 'short_url': data.short_url}
-    save_data(result)
-    return result, 'Ссылка добавлена!'
+@app.delete('/')
+def delete_url_handler(url: str) -> DataURLS:
+    return delete_data(url=url)
 
 
-@app.post('/delete_url')
-def delete_url_handler(short_url: str, long_url: str) -> tuple:
-    result = delete_data(short_url=short_url, long_url=long_url)
-    return result
+@app.put('/')
+def update_url_handler(new_url: str, old_url: str) -> DataURLS:
+    return update_data(new_url=new_url, old_url=old_url)
 
 
-@app.put('/update_url')
-def update_url_handler(new_url: str, old_url: str) -> tuple:
-    result = update_data(new_url=new_url, old_url=old_url)
-    return result
-
-
-@app.get('/show_all_urls')
-def show_all_urls_handler():
-    result = show_all_urls()
-    return result
+@app.get('/')
+def show_all_urls_handler() -> DataURLS:
+    return show_all_urls()
